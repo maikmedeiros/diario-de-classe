bfdc3c2760c24b3f0871ec546c02f2ca1f43c2b1
maikmedeiros <maikmedeirosm@outlook.com>
2023-07-25 23:06:13 -0300

    Testing

--- src/orm/models/pessoas.js
+++ src/orm/models/pessoas.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      pessoas.hasOne(models.logins, {
+      pessoas.hasOne(models.login, {
         foreignKey: { name: 'idPessoa', allowNull: false },
       });
     }
